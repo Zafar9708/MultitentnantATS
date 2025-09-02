228dd43e5201ee0be3aba2858f087c370c5dc722
Zafar9708 <aseem@kloudrac.com>
2025-09-02 17:43:15 +0530

    added proxy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   "compilerOptions": {
+//     "forceConsistentCasingInFileNames": true
+//   }
+  
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  "compilerOptions": {
-    "forceConsistentCasingInFileNames": true
-  }
-  
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.0.128:5000', // your backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
